f5f8e37917019bfa5d1ceeeb500b6ed14482c0f8
KhalidHasanSid <khalidhasansiddiqui21@gmail.com>
2025-01-23 16:22:55 +0500

    frontend load

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,25 @@
+import Header from "@/components/Header";
+import Header2 from "@/components/Header2";
+import Hero from "@/components/Hero";
+import Hero2 from "@/components/Hero2";
+import Hero4 from "@/components/Hero4";
+import Footer from "@/components/Footer"; // Use consistent casing
+import Footer1 from "@/components/footer1";
+import Footer2 from "@/components/footer2";
+
+
+export default function Home() {
+  return (
+    <div className="bg-gray-100 ">
+    <Header/>
+    <Header2/>
+    <Hero/>
+    <Hero2/>
+    <Hero4/>
+    <Footer/>
+    <Footer1/>
+    <Footer2/>
+
+    </div>
+  );
+}
